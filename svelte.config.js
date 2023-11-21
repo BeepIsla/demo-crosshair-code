9d9b234e55e5da3ef36896757cf0bc87ba78e5f8
BeepIsla <BeepIsla@gmail.com>
2023-11-21 17:13:08 +0100

    Maybe fix

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,10 @@ const config = {
 			pages: "build",
 			assets: "build",
 			fallback: null
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/demo-crosshair-code" : ""
+		}
 	}
 };
 
